64a27f60d76b188afc3f3cd8d241c112ebf22773
mikolalysenko <mikolalysenko@gmail.com>
2016-03-24 23:24:12 -0700

    add test case for triangle

---
+++ example/triangle.js
@@ -0,0 +1,32 @@
+var regl = require('../regl')()
+
+regl.clear({
+  color: [1, 0, 0, 1]
+})
+
+var drawTriangle = regl({
+  frag: [
+    'void main() {',
+    '  gl_FragColor = vec4(0, 0, 1, 1);',
+    '}'
+  ].join('\n'),
+
+  vert: [
+    'attribute vec4 position;',
+    'void main() {',
+    '  gl_Position = position;',
+    '}'
+  ].join('\n'),
+
+  attributes: {
+    position: regl.buffer([
+      [2, 2, 0, 1],
+      [2, -2, 0, 1],
+      [-2, -2, 0, 1]
+    ])
+  },
+
+  count: 3
+})
+
+drawTriangle()
---
+++ test/triangle.js
@@ -0,0 +1,54 @@
+'use strict'
+
+var createContext = require('gl')
+var createREGL = require('../../regl')
+var tape = require('tape')
+
+tape('triangle', function (t) {
+  var regl = createREGL(createContext(16, 16))
+
+  regl.clear({
+    color: [1, 0, 0, 1]
+  })
+
+  var drawTriangle = regl({
+    frag: [
+      'void main() {',
+      '  gl_FragColor = vec4(0, 0, 1, 1);',
+      '}'
+    ].join('\n'),
+
+    vert: [
+      'attribute vec4 position;',
+      'void main() {',
+      '  gl_Position = position;',
+      '}'
+    ].join('\n'),
+
+    attributes: {
+      position: regl.buffer([
+        [2, 2, 0, 1],
+        [2, -2, 0, 1],
+        [-2, -2, 0, 1]
+      ])
+    },
+
+    count: 3
+  })
+
+  drawTriangle()
+
+  var pixels = regl.read()
+
+  for (var i = 0; i < 16; ++i) {
+    for (var j = 0; j < 16; ++j) {
+      var ptr = 4 * (16 * i + j)
+      t.equals(pixels[ptr], i < j ? 255 : 0)
+      t.equals(pixels[ptr + 1], 0)
+      t.equals(pixels[ptr + 2], i < j ? 0 : 255)
+      t.equals(pixels[ptr + 3], 255)
+    }
+  }
+
+  t.end()
+})
