a83c370e0b88cfb219bea9c26e949dd5d10a8122
mikolalysenko <mikolalysenko@gmail.com>
2016-03-28 18:04:13 -0700

    add update case for elements

--- test/elements.js
+++ test/elements.js
@@ -11,8 +11,9 @@ tape('elements', function (t) {
 
   var drawStatic = regl({
     frag: [
+      'uniform vec4 color;',
       'void main() {',
-      '  gl_FragColor = vec4(0, 0, 1, 1);',
+      '  gl_FragColor = color;',
       '}'
     ].join('\n'),
 
@@ -65,7 +66,7 @@ tape('elements', function (t) {
     elements: regl.prop('elements')
   })
 
-  function testCanvas (msg) {
+  function testPlus (msg) {
     var pixels = regl.read()
     for (var i = 0; i < 16; ++i) {
       for (var j = 0; j < 16; ++j) {
@@ -79,21 +80,46 @@ tape('elements', function (t) {
     }
   }
 
+  function testBar (msg) {
+    var pixels = regl.read()
+    for (var i = 0; i < 16; ++i) {
+      for (var j = 0; j < 16; ++j) {
+        var ptr = 4 * (16 * i + j)
+        var hit = i === 7
+        t.equals(pixels[ptr], hit ? 0 : 255, msg)
+        t.equals(pixels[ptr + 1], 0, msg)
+        t.equals(pixels[ptr + 2], hit ? 255 : 0, msg)
+        t.equals(pixels[ptr + 3], 255, msg)
+      }
+    }
+  }
+
   regl.clear({ color: [1, 0, 0, 1] })
   drawStatic({c: [0, 0, 1, 1]})
-  testCanvas('draw - static')
+  testPlus('draw - static')
 
   regl.clear({ color: [1, 0, 0, 1] })
   drawStatic.batch([{c: [0, 0, 0, 1]}])
-  testCanvas('batch - static')
+  testPlus('batch - static')
 
   regl.clear({ color: [1, 0, 0, 1] })
   drawDynamic({elements: elements})
-  testCanvas('draw - dynamic')
+  testPlus('draw - dynamic')
 
   regl.clear({ color: [1, 0, 0, 1] })
   drawDynamic.batch([{elements: elements}])
-  testCanvas('batch - dynamic')
+  testPlus('batch - dynamic')
+
+  // try updating elements
+  elements([
+    [0, 2]
+  ])
+
+  drawStatic({c: [0, 0, 1, 1]})
+  testBar()
+
+  drawDynamic({elements: elements})
+  testBar()
 
   t.end()
 })
