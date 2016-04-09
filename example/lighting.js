5bdf6a4efaa99b11272ff5284b99450e3c14bc4a
mikolalysenko <mikolalysenko@gmail.com>
2016-04-09 16:16:52 -0700

    add a simple diffuse lighting example

---
+++ example/lighting.js
@@ -0,0 +1,105 @@
+var regl = require('../regl')()
+var normals = require('angle-normals')
+var mat4 = require('gl-mat4')
+var bunny = require('bunny')
+
+var drawBunny = regl({
+  vert: `
+  precision mediump float;
+  attribute vec3 position, normal;
+  uniform mat4 view, projection;
+  varying vec3 fragNormal, fragPosition;
+  void main() {
+    fragNormal = normal;
+    fragPosition = position;
+    gl_Position = projection * view * vec4(position, 1);
+  }`,
+
+  frag: `
+  precision mediump float;
+  struct Light {
+    vec3 color;
+    vec3 position;
+  };
+  uniform Light lights[4];
+  varying vec3 fragNormal, fragPosition;
+  void main() {
+    vec3 normal = normalize(fragNormal);
+    vec3 light = vec3(0, 0, 0);
+    for (int i = 0; i < 4; ++i) {
+      vec3 lightDir = normalize(lights[i].position - fragPosition);
+      float diffuse = max(0.0, dot(lightDir, normal));
+      light += diffuse * lights[i].color;
+    }
+    gl_FragColor = vec4(light, 1);
+  }`,
+
+  attributes: {
+    position: regl.buffer(bunny.positions),
+    normal: regl.buffer(normals(bunny.cells, bunny.positions))
+  },
+
+  elements: regl.elements(bunny.cells),
+
+  uniforms: {
+    view: function (args, batchId, stats) {
+      var t = 0.01 * stats.count
+      return mat4.lookAt([],
+        [30 * Math.cos(t), 2.5, 30 * Math.sin(t)],
+        [0, 2.5, 0],
+        [0, 1, 0])
+    },
+    projection: function (args, batchId, stats) {
+      return mat4.perspective([],
+        Math.PI / 4,
+        stats.width / stats.height,
+        0.01,
+        1000)
+    },
+    'lights[0].color': [1, 0, 0],
+    'lights[1].color': [0, 1, 0],
+    'lights[2].color': [0, 0, 1],
+    'lights[3].color': [1, 1, 0],
+    'lights[0].position': function (args, batchId, stats) {
+      var t = 0.1 * stats.count
+      return [
+        10 * Math.cos(t),
+        10 * Math.sin(2 * t),
+        10 * Math.cos(3 * t)
+      ]
+    },
+    'lights[1].position': function (args, batchId, stats) {
+      var t = 0.1 * stats.count
+      return [
+        10 * Math.cos(5 * t + 1),
+        10 * Math.sin(4 * t),
+        10 * Math.cos(0.1 * t)
+      ]
+    },
+    'lights[2].position': function (args, batchId, stats) {
+      var t = 0.1 * stats.count
+      return [
+        10 * Math.cos(9 * t),
+        10 * Math.sin(0.25 * t),
+        10 * Math.cos(4 * t)
+      ]
+    },
+    'lights[3].position': function (args, batchId, stats) {
+      var t = 0.1 * stats.count
+      return [
+        10 * Math.cos(0.3 * t),
+        10 * Math.sin(2.1 * t),
+        10 * Math.cos(1.3 * t)
+      ]
+    }
+  }
+})
+
+regl.frame(function () {
+  regl.clear({
+    depth: 1,
+    color: [0, 0, 0, 1]
+  })
+
+  drawBunny()
+})
