f1e149117d6bb0006a3771ba72a971d7cf1897ad
mikolalysenko <mikolalysenko@gmail.com>
2016-04-09 19:02:55 -0700

    add particle demo

---
+++ example/particles.js
@@ -0,0 +1,101 @@
+var regl = require('../regl')()
+var mat4 = require('gl-mat4')
+var hsv2rgb = require('hsv2rgb')
+
+var NUM_POINTS = 1e4
+var VERT_SIZE = 4 * (4 + 4 + 3)
+
+var pointBuffer = regl.buffer(Array(NUM_POINTS).fill().map(function () {
+  var color = hsv2rgb(Math.random() * 360, 0.6, 1)
+  return [
+    // freq
+    Math.random() * 10,
+    Math.random() * 10,
+    Math.random() * 10,
+    Math.random() * 10,
+    // phase
+    2.0 * Math.PI * Math.random(),
+    2.0 * Math.PI * Math.random(),
+    2.0 * Math.PI * Math.random(),
+    2.0 * Math.PI * Math.random(),
+    // color
+    color[0] / 255, color[1] / 255, color[2] / 255
+  ]
+}))
+
+var drawParticles = regl({
+  vert: `
+  precision mediump float;
+  attribute vec4 freq, phase;
+  attribute vec3 color;
+  uniform float time;
+  uniform mat4 view, projection;
+  varying vec3 fragColor;
+  void main() {
+    vec3 position = 8.0 * cos(freq.xyz * time + phase.xyz);
+    gl_PointSize = 5.0 * (1.0 + cos(freq.w * time + phase.w));
+    gl_Position = projection * view * vec4(position, 1);
+    fragColor = color;
+  }`,
+
+  frag: `
+  precision lowp float;
+  varying vec3 fragColor;
+  void main() {
+    if (length(gl_PointCoord.xy - 0.5) > 0.5) {
+      discard;
+    }
+    gl_FragColor = vec4(fragColor, 1);
+  }`,
+
+  attributes: {
+    freq: {
+      buffer: pointBuffer,
+      stride: VERT_SIZE,
+      offset: 0
+    },
+    phase: {
+      buffer: pointBuffer,
+      stride: VERT_SIZE,
+      offset: 16
+    },
+    color: {
+      buffer: pointBuffer,
+      stride: VERT_SIZE,
+      offset: 32
+    }
+  },
+
+  uniforms: {
+    view: function (args, batchId, stats) {
+      var t = 0.01 * stats.count
+      return mat4.lookAt([],
+        [30 * Math.cos(t), 2.5, 30 * Math.sin(t)],
+        [0, 0, 0],
+        [0, 1, 0])
+    },
+    projection: function (args, batchId, stats) {
+      return mat4.perspective([],
+        Math.PI / 4,
+        stats.width / stats.height,
+        0.01,
+        1000)
+    },
+    time: function (args, batchId, stats) {
+      return stats.count * 0.001
+    }
+  },
+
+  count: NUM_POINTS,
+
+  primitive: 'points'
+})
+
+regl.frame(function () {
+  regl.clear({
+    depth: 1,
+    color: [0, 0, 0, 1]
+  })
+
+  drawParticles()
+})
