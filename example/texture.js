2ec8f9c2c6c8f906208d28b815415a5abb60e264
mikolalysenko <mikolalysenko@gmail.com>
2016-04-17 15:41:15 -0700

    basic texture example working

---
+++ example/texture.js
@@ -0,0 +1,34 @@
+var regl = require('../regl')()
+var baboon = require('baboon-image')
+
+regl({
+  frag: `
+  precision mediump float;
+  uniform sampler2D texture;
+  varying vec2 uv;
+  void main () {
+    gl_FragColor = texture2D(texture, uv);
+  }`,
+
+  vert: `
+  precision mediump float;
+  attribute vec2 position;
+  varying vec2 uv;
+  void main () {
+    uv = position;
+    gl_Position = vec4(1.0 - 2.0 * position, 0, 1);
+  }`,
+
+  attributes: {
+    position: regl.buffer([
+      -2, 0,
+      0, -2,
+      2, 2])
+  },
+
+  uniforms: {
+    texture: regl.texture(baboon)
+  },
+
+  count: 3
+})()
