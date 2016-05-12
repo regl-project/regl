1fac57bbd2a15b6fbcd6914fd6906656a1dc0e06
mikolalysenko <mikolalysenko@gmail.com>
2016-05-12 11:06:08 -0700

    fix bug with order of initialization for context variables

--- test/context.js
+++ test/context.js
@@ -8,6 +8,11 @@ tape('context', function (t) {
   var simpleScope = regl({
     context: {
       a: function (props, context) {
+        t.equals(context.a, undefined)
+        t.equals(context.b, undefined)
+        t.equals(context.c, undefined)
+        t.equals(context.d, undefined)
+        t.equals(context.e, undefined)
         return 1
       },
       b: regl.this('bbb'),
@@ -22,6 +27,26 @@ tape('context', function (t) {
   }, {
     ddd: 7
   }, function (props, context) {
+    t.equals(context.a, 1)
+    t.equals(context.b, 3)
+    t.equals(context.c, 16)
+    t.equals(context.d, 7)
+    t.equals(context.e, 'eee')
+
+    regl({
+      context: {
+        b: 5,
+        c: 7,
+        d: regl.context('b')
+      }
+    }).call({}, {}, function () {
+      t.equals(context.a, 1)
+      t.equals(context.b, 5)
+      t.equals(context.c, 7)
+      t.equals(context.d, 3)
+      t.equals(context.e, 'eee')
+    })
+
     t.equals(context.a, 1)
     t.equals(context.b, 3)
     t.equals(context.c, 16)
