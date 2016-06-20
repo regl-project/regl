22b2aacd5186539d890881932a7a6b86b985ac53
mikolalysenko <mikolalysenko@gmail.com>
2016-06-20 11:00:42 -0700

    add microphone example

---
+++ example/microphone.js
@@ -0,0 +1,77 @@
+/* global AudioContext */
+const regl = require('../regl')()
+
+// First we need to get permission to use the microphone
+require('getusermedia')({audio: true}, function (err, stream) {
+  if (err) {
+    return
+  }
+
+  // Next we create an analyser node to intercept data from the mic
+  const context = new AudioContext()
+  const analyser = context.createAnalyser()
+
+  // And then we connect them together
+  context.createMediaStreamSource(stream).connect(analyser)
+
+  // Here we preallocate buffers for streaming audio data
+  const fftSize = analyser.frequencyBinCount
+  const frequencies = new Uint8Array(fftSize)
+  const fftBuffer = regl.buffer(frequencies)
+
+  // This command draws the spectrogram
+  const drawSpectrum = regl({
+    vert: `
+    precision mediump float;
+
+    #define FFT_SIZE ${fftSize}
+    #define PI ${Math.PI}
+
+    attribute float index, frequency;
+
+    void main() {
+      float theta = 2.0 * PI * index / float(FFT_SIZE);
+      gl_Position = vec4(
+        0.5 * cos(theta) * (1.0 + frequency),
+        0.5 * sin(theta) * (1.0 + frequency),
+        0,
+        1);
+    }`,
+
+    frag: `
+    void main() {
+      gl_FragColor = vec4(1, 1, 1, 1);
+    }`,
+
+    attributes: {
+      index: Array(fftSize).fill().map((_, i) => i),
+      frequency: {
+        buffer: fftBuffer,
+        normalized: true
+      }
+    },
+    elements: null,
+    instances: -1,
+    lineWidth: 1,
+    depth: {enable: false},
+    count: fftSize,
+    primitive: 'line loop'
+  })
+
+  regl.frame(({count}) => {
+    // Clear draw buffer
+    regl.clear({
+      color: [0, 0, 0, 1],
+      depth: 1
+    })
+
+    // Poll microphone data
+    analyser.getByteFrequencyData(frequencies)
+
+    // Here we use .subdata() to update the buffer in place
+    fftBuffer.subdata(frequencies)
+
+    // Draw the spectrum
+    drawSpectrum()
+  })
+})
