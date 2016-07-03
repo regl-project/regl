eb107a5570dddccb012cdc1bdc23a3252fec933e
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-03 19:45:32 +0200

    added sound effects.
    now the ball is reset when it hits the east or west walls.

--- example/pong.js
+++ example/pong.js
@@ -1,16 +1,12 @@
-// This example demonstrates how to use batch mode commands
-//
-// To use a command in batch mode, we pass in an array of objects.  Then
-// the command is executed once for each object in the array.
+/* global AudioContext */
 
-// As usual, we start by creating a full screen regl object
 const regl = require('../regl')()
 const vec2 = require('gl-vec2')
 
 var mouseY = null
 
-require('mouse-change')(function(buttons, x, y) {
-  mouseY = y;
+require('mouse-change')(function (buttons, x, y) {
+  mouseY = y
 })
 
 function Aabb (c, r) {
@@ -21,61 +17,174 @@ function Aabb (c, r) {
   this.r = vec2.fromValues(r[0], r[1])
 }
 
-function clamp(value, min, max) {
+function clamp (value, min, max) {
   return min < max
     ? (value < min ? min : value > max ? max : value)
     : (value < max ? max : value > min ? min : value)
 }
 
-//443pong.js [sm]:31 x0 = -0.0007134545594453812 x1 = -0.09928654320538044 y0 = -0.19452300108969212 y1 = -0.14547700993716717
-function detectAabbCollision(a, b, r) {
-
+function detectAabbCollision (a, b, r) {
   var x0 = +(a.c[0] - b.c[0]) - (a.r[0] + b.r[0])
   var x1 = -(a.c[0] - b.c[0]) - (a.r[0] + b.r[0])
   if (x0 > 0.0 || x1 > 0.0) return false
 
-  var y0 = +(a.c[1] - b.c[1]) - r*(a.r[1] + b.r[1])
-  var y1 = -(a.c[1] - b.c[1]) - r*(a.r[1] + b.r[1])
+  var y0 = +(a.c[1] - b.c[1]) - r * (a.r[1] + b.r[1])
+  var y1 = -(a.c[1] - b.c[1]) - r * (a.r[1] + b.r[1])
   if (y0 > 0.0 || y1 > 0.0) return false
 
   // a and b overlap on all axes. So we have collision.
   // Now we need to find the contact normal.
 
-  if(x0 > x1 && x0 > y0 && x0 > y1) {
-    return vec2.fromValues(-1.0, 0.0);
+  if (x0 > x1 && x0 > y0 && x0 > y1) {
+    return vec2.fromValues(-1.0, 0.0)
   }
-  if(x1 > x0 && x1 > y0 && x1 > y1) {
-    return vec2.fromValues(+1.0, 0.0);
-  }
-
-  if(y0 > x0 && y0 > x1 && y0 > y1) {
-    return vec2.fromValues(0.0, -1.0);
-  }
-  if(y1 > x0 && y1 > x1 && y1 > y0) {
-    return vec2.fromValues(0.0, +1.0);
+  if (x1 > x0 && x1 > y0 && x1 > y1) {
+    return vec2.fromValues(+1.0, 0.0)
   }
 
-
-  console.log("WTF");
-
-
-  return [];
-}
-
-function reflect (v, n) {
-  var scratch = [0.0, 0.0]
-
-  var cr = 1.1
-  return vec2.subtract(v, v, vec2.scale(scratch, n, (1.0 + cr) * vec2.dot(v, n)))
+  if (y0 > x0 && y0 > x1 && y0 > y1) {
+    return vec2.fromValues(0.0, -1.0)
+  }
+  if (y1 > x0 && y1 > x1 && y1 > y0) {
+    return vec2.fromValues(0.0, +1.0)
+  }
 }
 
 var playerPaddle = new Aabb([-0.9, 0.0], [0.03, 0.15])
 var aiPaddle = new Aabb([+0.9, 0.0], [0.03, 0.15])
 
 var midline = new Aabb([+0.0, 0.0], [0.005, 1.0])
-var ball = new Aabb([+0.4, 0.5], [0.02, 0.02])
 
-var ballVel = vec2.fromValues(0.2, 0.2)
+// we set all ball properties in resetBall()
+var ball = new Aabb([0.0, 0.0], [0.0, 0.0]) // set velocity
+var ballVel = vec2.fromValues(0.0, 0.0)
+
+const context = new AudioContext()
+const volume = 0.1
+
+function getRand(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+function resetBall(playerWon) {
+  ball.c = [+0.0, 0.0]
+  ball.r = [0.02, 0.02]
+
+  var speed = getRand(0.3, 0.4)
+  const RANGE = 1.2
+
+  if(!playerWon) {
+    var theta = getRand(-RANGE, +RANGE)
+    ballVel = [speed * Math.cos(theta), speed * Math.sin(theta)]
+  } else {
+    var theta = getRand(Math.PI - RANGE, Math.PI + RANGE)
+    ballVel = [speed * Math.cos(theta), speed * Math.sin(theta)]
+  }
+}
+
+// create audio buffer that lasts `length` seconds, and `createAudioDataCallback`
+// will will fill each of the two channels of the buffer with audio data.
+function createAudioBuffer (length, createAudioDataCallback) {
+  var channels = 2
+  var frameCount = context.sampleRate * length
+  var audioBuffer = context.createBuffer(channels, frameCount, context.sampleRate)
+
+  for (var channel = 0; channel < channels; channel++) {
+    var channelData = audioBuffer.getChannelData(channel)
+    createAudioDataCallback(channelData, frameCount)
+  }
+  return audioBuffer
+}
+
+function playAudioBuffer(audioBuffer) {
+  // Appearently, you have to create a new AudioBufferSourceNode
+  // every time you want to play a sound again.
+  var source = context.createBufferSource()
+  source.buffer = audioBuffer
+  source.connect(context.destination)
+  source.start()
+}
+
+// When the ball collides with something, we alternate between playing two sound effects
+// Both sound effects are just simple square waves.
+var hitAudioBuffers = []
+
+hitAudioBuffers[0] =
+    createAudioBuffer(0.15,
+                      (channelData, frameCount) => {
+                        var current = volume
+                        for (var i = 0; i < frameCount; i++) {
+                          if (i % 100 === 0) {
+                            current *= -1.0
+                          }
+                          channelData[i] = current * (1.0 - i / frameCount)
+                        }
+                      })
+
+hitAudioBuffers[1] =
+    createAudioBuffer(0.15,
+                      (channelData, frameCount) => {
+                        var current = volume
+                        for (var i = 0; i < frameCount; i++) {
+                          if (i % 150 === 0) {
+                            current *= -1.0
+                          }
+                          channelData[i] = current * (1.0 - i / frameCount)
+                        }
+                      })
+
+
+
+// We play this sound when the player wins.
+// It is just a sqauare wave, with some simple frequency modulation.
+var winAudioBuffer =
+    createAudioBuffer(0.4,
+                      (channelData, frameCount) => {
+                        var current = volume
+                        var period = 50
+                        for (var i = 0; i < frameCount; i++) {
+                          if (i % period === 0) {
+                            current *= -1.0
+                          }
+                          if(i % 600 === 0) {
+                            period -= 2
+                          }
+                          var a = (i / frameCount)
+                          channelData[i] = current * (1.0 - a)
+                        }
+                      })
+
+
+
+// We play this sound when the player loses.
+// It is just white noise.
+var loseAudioBuffer =
+    createAudioBuffer(0.5,
+                      (channelData, frameCount) => {
+
+                        var current = getRand(-volume, +volume)
+                        for (var i = 0; i < frameCount; i++) {
+
+                          if (i % 150 === 0) {
+                            current = getRand(-volume, +volume)
+                          }
+                          channelData[i] = current * (1.0 - i / frameCount)
+                        }
+                      })
+
+var iHitAudioBuffer = 0
+
+
+function reflect (v, n) {
+  var scratch = [0.0, 0.0]
+
+  playAudioBuffer(hitAudioBuffers[iHitAudioBuffer])
+  iHitAudioBuffer = (iHitAudioBuffer + 1) % 2
+
+  var cr = 1.1
+  return vec2.subtract(v, v, vec2.scale(scratch, n, (1.0 + cr) * vec2.dot(v, n)))
+}
 
 // Next we create our command
 const drawAabb = regl({
@@ -127,6 +236,11 @@ const drawAabb = regl({
   count: 6
 })
 
+// initialize game.
+resetBall(true)
+
+//playAudioBuffer(winAudioBuffer)
+
 regl.frame(function ({deltaTime, viewportWidth, viewportHeight, pixelRatio}) {
   regl.clear({
     color: [0, 0, 0, 1]
@@ -138,49 +252,54 @@ regl.frame(function ({deltaTime, viewportWidth, viewportHeight, pixelRatio}) {
   //
 
   // player paddle follows the mouse
-  if(mouseY !== null) {
+  if (mouseY !== null) {
     var a = 1.0 - (mouseY * pixelRatio) / viewportHeight
-    playerPaddle.c[1] = clamp(-1.0 + 2.0 * (a), -1 + playerPaddle.r[1]*r, +1 - playerPaddle.r[1]*r )
+    playerPaddle.c[1] = clamp(-1.0 + 2.0 * (a), -1 + playerPaddle.r[1] * r, +1 - playerPaddle.r[1] * r)
   }
 
   // AI paddle follows the ball.
   var dist = (ball.c[1] - aiPaddle.c[1])
-  aiPaddle.c[1] = clamp(aiPaddle.c[1] + dist*deltaTime*1.9, -1 + playerPaddle.r[1]*r, +1 - playerPaddle.r[1]*r )
+  aiPaddle.c[1] = clamp(aiPaddle.c[1] + dist * deltaTime * 1.9, -1 + playerPaddle.r[1] * r, +1 - playerPaddle.r[1] * r)
 
   // Move ball.
   vec2.scaleAndAdd(ball.c, ball.c, ballVel, deltaTime)
 
   // Handle ball collision north wall
-  if ((ball.c[1] + r*ball.r[1]) >= 1.0) {
+  if ((ball.c[1] + r * ball.r[1]) >= 1.0) {
     ballVel = reflect(ballVel, vec2.fromValues(0.0, -1.0))
   }
 
   // Handle ball collision east wall
   if ((ball.c[0] + ball.r[0]) >= 1.0) {
-    ballVel = reflect(ballVel, vec2.fromValues(-1.0, 0.0))
+    playAudioBuffer(winAudioBuffer)
+    // player win. Reset ball
+    resetBall(true)
+
   }
 
   // Handle ball collision south wall
-  if ((ball.c[1] - r*ball.r[1]) <= -1.0) {
+  if ((ball.c[1] - r * ball.r[1]) <= -1.0) {
     ballVel = reflect(ballVel, vec2.fromValues(0.0, 1.0))
   }
 
   // Handle ball collision west wall
   if ((ball.c[0] - ball.r[0]) <= -1.0) {
-    ballVel = reflect(ballVel, vec2.fromValues(1.0, 0.0))
+    playAudioBuffer(loseAudioBuffer)
+    // player loss. Reset ball.
+    resetBall(false)
   }
 
   // handle ball and AI paddle collision
-  var result = detectAabbCollision(aiPaddle, ball, r);
-  if(result !== false) {
-    var n = result; // if collision, the return value is the contact normal.
+  var result = detectAabbCollision(aiPaddle, ball, r)
+  if (result !== false) {
+    var n = result // if collision, the return value is the contact normal.
     ballVel = reflect(ballVel, n)
   }
 
   // handle ball and player paddle collision
-  result = detectAabbCollision(playerPaddle, ball, r);
-  if(result !== false) {
-    n = result; // if collision, the return value is the contact normal.
+  result = detectAabbCollision(playerPaddle, ball, r)
+  if (result !== false) {
+    n = result // if collision, the return value is the contact normal.
     ballVel = reflect(ballVel, n)
   }
 
