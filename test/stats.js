67bd8478e72712cc570709b5d96f8ccdff311631
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-04 11:02:24 +0200

    made test/stats.js less verbose.

--- test/stats.js
+++ test/stats.js
@@ -8,23 +8,25 @@ tape('test regl.stats', function (t) {
   setTimeout(function () {
     var gl = createContext(16, 16)
     var regl
+    var stats
 
     //
     // Begin Test stats.bufferCount
     //
     regl = createREGL(gl)
+    stats = regl.stats
 
-    t.equals(regl.stats.bufferCount, 0, 'stats.bufferCount==0 at start')
+    t.equals(stats.bufferCount, 0, 'stats.bufferCount==0 at start')
 
     regl.buffer([1, 2, 3])
     regl.buffer(new Uint16Array([1, 2, 3]))
     regl.buffer(new Float32Array([1, 2, 3, 4]))
 
-    t.equals(regl.stats.bufferCount, 3, 'stats.bufferCount==3 after creating 3 buffers')
+    t.equals(stats.bufferCount, 3, 'stats.bufferCount==3 after creating 3 buffers')
 
     regl.destroy()
 
-    t.equals(regl.stats.bufferCount, 0, 'stats.bufferCount==0 after regl.destroy()')
+    t.equals(stats.bufferCount, 0, 'stats.bufferCount==0 after regl.destroy()')
     //
     // End Test stats.bufferCount
     //
@@ -34,7 +36,7 @@ tape('test regl.stats', function (t) {
     //
     regl = createREGL(gl)
 
-    t.equals(regl.stats.elementsCount, 0, 'stats.elementsCount==0 at start')
+    t.equals(stats.elementsCount, 0, 'stats.elementsCount==0 at start')
 
     regl.elements([1, 2, 3])
     regl.elements([[1, 2, 3], [5, 6, 7]])
@@ -44,11 +46,11 @@ tape('test regl.stats', function (t) {
       data: new Uint8Array([0, 2, 4, 1, 3])
     })
 
-    t.equals(regl.stats.bufferCount, 3, 'stats.elementsCount==3 after creating 3 buffers')
+    t.equals(stats.bufferCount, 3, 'stats.elementsCount==3 after creating 3 buffers')
 
     regl.destroy()
 
-    t.equals(regl.stats.elementsCount, 0, 'stats.elementsCount==0 after regl.destroy()')
+    t.equals(stats.elementsCount, 0, 'stats.elementsCount==0 after regl.destroy()')
     //
     // End Test stats.bufferCount
     //
