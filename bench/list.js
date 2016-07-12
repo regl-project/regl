687c9c80bda978b00a4aba7582c95ceb2f7c3b85
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-12 06:34:38 +0200

    made cube webgl benchmark.

--- bench/list.js
+++ bench/list.js
@@ -1,6 +1,7 @@
 module.exports = {
   'clear': {proc: require('./clear'), warmupSamples: 10, samples: 100},
-  'cube': {proc: require('./cube'), warmupSamples: 50, samples: 1000}
+  'cube': {proc: require('./cube'), warmupSamples: 50, samples: 1000},
+  'cube_webgl': {proc: require('./cube_webgl'), warmupSamples: 50, samples: 1000}
   // 'buffer': require('./buffer'),
   // 'draw-static': require('./draw-static'),
   // 'draw-dynamic': require('./draw-dynamic'),
