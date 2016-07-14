cee1ee19d9cde2cf3b2a57f10cd2711ba58c26ca
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-14 08:19:53 +0200

    Now drawing lots of random bunnies in `deferred_shading.js`

--- example/deferred_shading.js
+++ example/deferred_shading.js
@@ -205,11 +205,15 @@ const drawPointLight = regl({
     model: (_, props, batchId) => {
       var m = mat4.identity([])
 
+
+
       mat4.translate(m, m, props.translate)
       var s = props.scale
 
       var r = props.radius
       mat4.scale(m, m, [r, r, r])
+
+
 
       return m
     }
@@ -226,10 +230,16 @@ const drawPointLight = regl({
       dst: 'one'
     },
   },
-
+  cull: {
+    enable: true
+  },
+  // We render only the inner faces of the light sphere.
+  // In other words, we render the back-faces and not the front-faces of the sphere.
+  // If we render the front-faces, the lighting of the light sphere disappears if
+  // we are inside the sphere, which is weird. But by rendering the back-faces instead,
+  // we solve this problem.
+  frontFace: 'cw',
 })
-
-
 
 function Mesh (elements, position, normal) {
   this.elements = elements
@@ -242,6 +252,7 @@ Mesh.prototype.draw = regl({
     model: (_, props, batchId) => {
       var m = mat4.identity([])
 
+
       mat4.translate(m, m, props.translate)
       var s = props.scale
 
@@ -250,6 +261,10 @@ Mesh.prototype.draw = regl({
       } else { // else, we assume an array
         mat4.scale(m, m, s)
       }
+
+      if(typeof props.yRotate !== 'undefined')
+        mat4.rotateY(m, m, props.yRotate)
+
 
       return m
     },
@@ -270,7 +285,24 @@ Mesh.prototype.draw = regl({
 var bunnyMesh = new Mesh(bunny.cells, bunny.positions, normals(bunny.cells, bunny.positions))
 var boxMesh = new Mesh(boxElements, boxPosition, boxNormal)
 
+
+
 //console.log("l: ", sphereMesh.positions, sphereMesh.normals, sphereMesh.cells)
+
+function randomRange (min, max) {
+  var t = Math.random()
+  return min * (1.0 - t) + max * t
+}
+
+function randomColor (c, variance) {
+  return [
+    c[0] + randomRange(-variance, +variance),
+    c[1] + randomRange(-variance, +variance),
+    c[2] + randomRange(-variance, +variance),
+  ]
+}
+
+
 
 regl.frame(({tick, viewportWidth, viewportHeight}) => {
   regl.updateTimer()
@@ -284,37 +316,123 @@ regl.frame(({tick, viewportWidth, viewportHeight}) => {
         depth: 1
       })
 
-      boxMesh.draw({scale: 4.2, translate: [0.0, 9.0, 0], color: [0.05, 0.5, 0.5]})
-      bunnyMesh.draw({scale: 0.7, translate: [0.0, 3.3, 8.0], color: [0.55, 0.2, 0.05]})
-      bunnyMesh.draw({scale: 0.8, translate: [-10, 3.3, 0.0], color: [0.55, 0.55, 0.05]})
-      bunnyMesh.draw({scale: 0.8, translate: [-40, 3.3, 0.0], color: [0.55, 0.55, 0.05]})
-      bunnyMesh.draw({scale: 0.8, translate: [+60, 3.3, 0.0], color: [0.55, 0.55, 0.97]})
-
-      var S = 400 // box size
+      var S =800 // box size
       var T = 0.1 // box wall thickness
       var C = [0.45, 0.45, 0.45] // box color
 
-      boxMesh.draw({scale: [S, T, S], translate: [0.0, 0.0, 0], color: C})
+      var N_BUNNIES = 5
 
+      var bunnies = []
+
+      // modulo that works for negative numbers
+      function negMod (x, n) {
+        return ((x % n) + n) % n
+      }
+
+      for (x = -N_BUNNIES; x <= +N_BUNNIES; x++) {
+
+        for (z = -N_BUNNIES; z <= +N_BUNNIES; z++) {
+
+          var xs = x / (N_BUNNIES+1)
+          var zs = z / (N_BUNNIES+1)
+
+          // pseudo-random color
+          var c = [
+            ((Math.abs(3 * x + 5 * z + 100) % 10) / 10) * 0.84,
+            ((Math.abs(64 * x + x * z + 23) % 13) / 13) * 0.87,
+            ((Math.abs(143 * x * z + x * z * z + 19) % 11) / 11) * 0.91,
+          ]
+
+          var A = S / 20 // max bunny displacement amount.
+
+          // random displavement
+          var xd = (negMod(z *z * 231 + x * x * 343, 24) / 24) * 0.97 * A
+          var zd = (negMod(z * x * 198 + x * x * z * 24, 25) / 25) * 0.987 * A
+
+
+          var s = ((Math.abs(3024 * z + 5239 * x + 1321) % 50) / 50) * 3.4 + 0.9
+
+          var r = ((Math.abs(9422 * z*x + 3731 * x*x + 2321) % 200) / 200) * 2 * Math.PI
+
+          bunnies.push({scale: s, translate: [xs * S / 2.0+ xd, -0.2, zs * S / 2.0 + zd], color: c, yRotate: r})
+        }
+
+      }
+
+      bunnyMesh.draw(bunnies)
+      //      boxMesh.draw({scale: 4.2, translate: [0.0, 9.0, 0], color: [0.05, 0.5, 0.5]})¨
+
+
+  /*
+      bunnyMesh.draw({scale: 0.7, translate: [0.0, -0.2, 8.0], color: [0.55, 0.2, 0.05]})
+      bunnyMesh.draw({scale: 0.8, translate: [-10, -0.2, 0.0], color: [0.55, 0.55, 0.05]})
+      bunnyMesh.draw({scale: 0.8, translate: [-40, -0.2, 0.0], color: [0.55, 0.55, 0.05]})
+      bunnyMesh.draw({scale: 0.8, translate: [+60, -0.2, 0.0], color: [0.55, 0.55, 0.97]})
+*/
+
+      boxMesh.draw({scale: [S, T, S], translate: [0.0, 0.0, 0], color: C})
       boxMesh.draw({scale: [T, S, S], translate: [S / 2, S / 2, 0], color: C})
       boxMesh.draw({scale: [T, S, S], translate: [-S / 2, S / 2, 0], color: C})
-
       boxMesh.draw({scale: [S, S, T], translate: [0, S / 2, S / 2], color: C})
       boxMesh.draw({scale: [S, S, T], translate: [0, S / 2, -S / 2], color: C})
     })
 
     drawDirectionalLight()
 
+    pointLights = []
+
+    var N = 10
+
+    for(var i = 0; i < N; ++i) {
+      var theta = 2 * Math.PI * i * (1.0 / (N));
+      theta += tick * 0.01
+      var R = 100
+
+      var mod = i % 3
+      var r,g,b
+      var a = 0.8
+      r = 0.2
+      b = 0.7
+
+      if (mod === 0) {
+        g = 0.2
+      } else if (mod === 1) {
+        g = 0.7
+      } else if (mod === 2) {
+        g = 0.5
+      }
+
+      mod = (i+2) % 3
+
+      if (mod === 0) {
+        r = 0.1
+      } else if (mod === 1) {
+        r = 0.9
+      } else if (mod === 2) {
+        r = 0.5
+      }
+
+      mod = (i+9) % 4
+
+      if (mod === 0) {
+        b = 0.6
+      } else if (mod === 1) {
+        r = 0.24
+      } else if (mod === 2) {
+        r = 1.0
+      } else if (mod === 3) {
+        r = 0.1
+      }
+
+//      pointLights.push({radius:30.0, translate: [R * Math.sin(theta), 0.0, R * Math.cos(theta)], ambientLight: [a * r, a * g, a * b], diffuseLight: [r, g, b]})
+    }
+
     pointLights = [
       {radius:20.0, translate: [0.0, 0.0, 0.0], ambientLight: [0.4, 0.0, 0.0], diffuseLight: [0.6, 0.0, 0.0]},
       {radius:20.0, translate: [60.0, 0.0, 0.0], ambientLight: [0.0, 0.2, 0.0], diffuseLight: [0.0, 0.6, 0.0]}
-
     ]
 
     drawPointLight(pointLights)
-
-//	GL_C(glBlendFunc(GL_ONE, GL_ONE));
-
   })
 
   camera.tick()
