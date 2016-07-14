0a0ff04342365c38b0b1675480d963cf608bc9d8
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-14 15:47:58 +0200

    `bench/index.js` in headless now outputs the benchmark data as json to stdout.
    Fixed so that the memory info is outputted when running the same script in browser.

--- bench/list.js
+++ bench/list.js
@@ -1,9 +1,9 @@
 module.exports = {
-  'clear': {proc: require('./clear'), warmupSamples: 1000, samples: 10000},
+  'clear': {proc: require('./clear'), warmupSamples: 1000, samples: 30000},
   'cube': {proc: require('./cube'), warmupSamples: 3000, samples: 30000},
   'cube-webgl': {proc: require('./cube-webgl'), warmupSamples: 3000, samples: 30000},
   'buffer': {proc: require('./buffer'), warmupSamples: 3000, samples: 30000},
   'draw-static': {proc: require('./draw-static'), warmupSamples: 3000, samples: 30000},
-  'draw-dynamic': {proc: require('./draw-dynamic'), warmupSamples: 3000, samples: 30008},
-  'draw-batch': {proc: require('./draw-batch'), warmupSamples: 3000, samples: 30008}
+  'draw-dynamic': {proc: require('./draw-dynamic'), warmupSamples: 3000, samples: 30000},
+  'draw-batch': {proc: require('./draw-batch'), warmupSamples: 3000, samples: 30000}
 }
