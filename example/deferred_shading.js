32e642d6febddff722ae8f6d821a230c58167dda
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-14 18:10:27 +0200

    add explanatory comment to `deferred_shading.js`

--- example/deferred_shading.js
+++ example/deferred_shading.js
@@ -1,3 +1,13 @@
+/*
+  This example is a simple implementation of deferred shading.
+
+  The focus of this implementation was readability, so it is not an
+  optimized implementation, and can certainly be made more efficient.
+  (by for instance getting rid of the "position" render target.
+  It can be computed from the depth buffer. )
+
+  This example demonstrates the usage of Multiple-render targets in regl.
+ */
 const webglCanvas = document.body.appendChild(document.createElement('canvas'))
 const fit = require('canvas-fit')
 const regl = require('../regl')(webglCanvas)
