c4389ffb0a1813adbd717d58a1eba320a2ccce1e
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-17 10:17:27 +0200

    Cleaned up the bench-graph.js script.

--- bench/bench-graph.js
+++ bench/bench-graph.js
@@ -1,4 +1,32 @@
-document.write(
+var fs = require('fs')
+const execSync = require('child_process').execSync
+
+var jsonFile
+// command line parsing.
+var args = process.argv.slice(2)
+if (args.length > 0) {
+  jsonFile = args[0]
+} else {
+  console.log('Please specify JSON test data.')
+  process.exit(1)
+}
+
+try {
+  var json = fs.readFileSync(jsonFile)
+} catch (e) {
+  console.log('Could not read json file: ', jsonFile)
+}
+
+// create output file name
+var i = jsonFile.lastIndexOf('.')
+var outputFile
+if (i === -1) {
+  outputFile = jsonFile + '.html'
+} else {
+  outputFile = jsonFile.slice(0, -(jsonFile.length - i)) + '.html'
+}
+
+fs.writeFileSync(outputFile,
   `
     <!DOCTYPE html>
     <meta charset="utf-8">
@@ -57,6 +85,188 @@ text {
   <script src="https://d3js.org/d3.v3.min.js"></script>
   <script src="http://momentjs.com/downloads/moment.min.js"></script>
 
-  <script src="bench/create-graph.js"></script>
+  <script>
+  ` +
+
+`
+function createGraph (json) {
+  function sigfigs (x) {
+    var xr = Math.round(x * 1000)
+    return (xr / 1000)
+  }
+
+  // Setup margins.
+  var margin = {top: 30, right: 20, bottom: 30, left: 50}
+  var width = 600 - margin.left - margin.right
+  var height = 270 - margin.top - margin.bottom
+
+  var x = d3.time.scale().range([0, width])
+  var y = d3.scale.linear().range([height, 0])
+
+  var xAxis = d3.svg.axis().scale(x)
+      .orient('bottom').ticks(5)
+  var yAxis = d3.svg.axis().scale(y)
+      .orient('left').ticks(5)
+
+  var valueline = d3.svg.line()
+      .x(function (d) { return x(d.date) })
+      .y(function (d) { return y(d.testData.time.mean) })
+
+  // Define the div for the tooltip
+  var div = d3.select('body').append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0) // initially, the div is invisible.
+
+  Object.keys(json[0].testData).map(function (testCase) {
+    if (testCase !== 'cube') {
+      return
+    }
+
+    // create header.
+    var header = document.createElement('h1')
+    header.innerHTML = testCase
+    document.body.appendChild(header)
+
+    // gather test data for this test case.
+    var data = []
+    for (var i = 0; i < json.length; i++) {
+      data.push({
+        date: new Date(json[i].timestamp * 1000),
+        title: json[i].title,
+        description: json[i].description,
+        hash: json[i].hash,
+        author: json[i].author,
+
+        testData: json[i].testData[testCase]
+      })
+    }
+
+    // add svg canvas.
+    var svg = d3.select('body')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform',
+              'translate(' + margin.left + ',' + margin.top + ')')
+
+    x.domain(d3.extent(data, function (d) { return d.date }))
+    y.domain([0, d3.max(data, function (d) { return d.testData.time.mean })])
+
+    // draw line chart.
+    svg.append('path')
+      .attr('class', 'line')
+      .attr('d', valueline(data))
+
+    // draw data point dots.
+    svg.selectAll('dot')
+      .data(data)
+      .enter()
+      .append('a')
+      .attr('xlink:href',
+            function (d) {
+              return 'https://github.com/mikolalysenko/regl/commit/' + d.hash
+            })
+      .attr('target', '"_blank"')
+      .append('circle')
+
+      .attr('r', 5)
+      .attr('cx', function (d) { return x(d.date) })
+      .attr('cy', function (d) { return y(d.testData.time.mean) })
+
+    // show tooltip on hover.
+      .on('mouseover', function (d) {
+        div
+          .transition()
+          .duration(100)
+          .style('opacity', 0.9)
+
+        var desc = d.title + d.description
+        var shortenedDesc = desc.length > 70 ? desc.substring(0, 69) + '...' : desc
+        var commitUrl = 'https://github.com/mikolalysenko/regl/commit/' + d.hash
+
+        var timeDiff = moment(d.date).fromNow()
+
+        console.log('link: ', commitUrl)
+
+        div.html(
+          '<table>' +
+            '<tbody>' +
+
+          '<tr>' +
+            '<td><b>Hash: </b></td>  <td>' + '<code>' + d.hash.substring(0, 7) + '</code>' + '</td>' +
+            '</tr>' +
+
+          '<tr>' +
+            '<td><b>Desc.: </b></td>  <td>' + shortenedDesc + '</td>' +
+            '</tr>' +
+
+          '<tr>' +
+            '<td><b>Time: </b></td>  <td>' + sigfigs(d.testData.time.mean) + '∓' + sigfigs(d.testData.time.stddev) + 'ms' + '</td>' +
+            '</tr>' +
+
+          '<tr>' +
+            '<td><b>Author: </b></td>  <td>' + d.author + '</td>' +
+            '</tr>' +
+
+          '<tr>' +
+            '<td><b>Date: </b></td>  <td>' + timeDiff + '</td>' +
+            '</tr>' +
+
+          '</tbody>' +
+            '</table>'
+        )
+          .style('left', (d3.event.pageX + 10) + 'px')
+          .style('top', (d3.event.pageY - 28) + 'px')
+      }).on('mouseout',
+            function (d) {
+              div.transition()
+                .duration(100)
+                .style('opacity', 0)
+            })
+
+    // X-axis
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis)
+      .append('text')
+      .attr('class', 'label')
+      .attr('x', width + 20)
+      .attr('y', -6)
+      .style('text-anchor', 'end')
+      .text('Commit Time')
+
+    // Y-axis
+    svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis)
+      .append('text')
+      .attr('class', 'label')
+      .attr('y', -20)
+      .attr('x', 40)
+      .attr('dy', '.7em')
+      .style('text-anchor', 'end')
+      .text('Runtime(ms)')
+  })
+}
+
+` + '\n' +
+'createGraph(' + json + ')' +
+  `
+  </script>
+
   </body>
     `)
+
+// next, we open the output file in the default web browser.
+var open
+var plat = process.platform
+if (plat === 'darwin') {
+  open = 'open'
+} else if (plat === 'win32' || plat === 'win64') {
+  open = 'open'
+} else {
+  open = 'xdg-open'
+}
+execSync(open + ' ' + outputFile)
