a04bda47d6f3e748725a8fc82fa8be839a532eb9
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-17 12:27:35 +0200

    Now `bench-history.js` can't collect benchmark info beyond the ancestor commit.
    Now `bench-history.js` can take command line arguments.

--- bench/bench-history.js
+++ bench/bench-history.js
@@ -2,22 +2,44 @@ var parseCommit = require('git-parse-commit')
 const execSync = require('child_process').execSync
 const os = require('os')
 var fs = require('fs')
-const crypto = require('crypto');
+const crypto = require('crypto')
+
+// num commits to collect benchmarking info for.
+var numCommits = 5
+
+// parse command line.
+var args = process.argv.slice(2)
+if (args.length > 0) {
+  numCommits = parseInt(args[0])
+
+  if (isNaN(numCommits)) {
+    console.log('Command line argument must be an integer.')
+    process.exit(1)
+  }
+}
+
+var quietMode = true
 
 // gather device info.
 var deviceInfo = {}
-
 deviceInfo['cpu'] = os.cpus()[0].model
 deviceInfo['platform'] = os.platform()
 deviceInfo['release'] = os.release()
 deviceInfo['arch'] = os.arch()
 
+/*
+  We do not allow collecting benchmarking info before this commit.
+  Before this commit, the benchmarks were not yet properly set up, so
+  they not produce reasonable benchmarking data.
+ */
+var ancestorCommit = 'e1799c0055b07f3722f8635c2a9bf7759e9d11e8'
+
 // Record original branch name
 var originalCommit = execSync('git rev-parse --abbrev-ref HEAD') + ''
-console.log('Original commit name:', originalCommit)
+console.log('') // insert newline
 
 // get a list of commits, and split them based on the null-character.
-var output = execSync('git rev-list HEAD --max-count 1 --header') + ''
+var output = execSync('git rev-list HEAD --max-count ' + numCommits + ' --header') + ''
 var commits = output.split('\u0000')
 commits.splice(-1, 1) // last element is will be an empty string, so remove it.
 
@@ -29,7 +51,15 @@ var testResults = []
 for (var i = 0; i < commits.length; i++) {
   var commit = parseCommit(commits[i])
 
-  execSync('git checkout ' + commit.hash)
+  try {
+    execSync('git merge-base --is-ancestor ' + ancestorCommit + ' ' + commit.hash)
+  } catch (e) {
+    console.log('  We reached the ancestor commit ' + ancestorCommit)
+    console.log('  Stop collecting data')
+    break
+  }
+
+  execSync('git checkout ' + commit.hash + (quietMode ? ' -q' : ''))
 
   try {
     // run benchmark. The results of the benchmark script is sent to stdout as json.
@@ -44,10 +74,9 @@ for (var i = 0; i < commits.length; i++) {
       testData: json
     }
     testResults.push(obj)
-    console.log("Collected benchmark results for commit ", commit.hash)
-
+    console.log('  Collected benchmark results for commit ', commit.hash)
   } catch (e) {
-    console.log("WARNING: could not run benchmarks for commit ", commit.hash)
+    console.log('  WARNING: could not run benchmarks for commit ', commit.hash)
   }
 }
 
@@ -66,9 +95,8 @@ result['deviceInfo'] = deviceInfo
 // The name of the file is based on a randomly generated ID-value
 
 var jsonStr = JSON.stringify(result)
-var id = crypto.randomBytes(10).toString('hex');
+var id = crypto.randomBytes(10).toString('hex')
 var outputFile = 'bench/bench-result-' + id + '.json'
 
-
 fs.writeFileSync(outputFile, jsonStr)
-console.log("Benchmarking data written to " + outputFile)
+console.log('  Benchmarking data written to ' + outputFile)
