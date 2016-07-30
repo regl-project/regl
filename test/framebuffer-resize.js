f523e6848c4584718d534897d9e318e393a30425
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-30 11:56:22 +0200

    test/framebuffer-resize.js: Refactor and remove cube duplication.

--- test/framebuffer-resize.js
+++ test/framebuffer-resize.js
@@ -9,34 +9,31 @@ tape('framebuffer resizing', function (t) {
     optionalExtensions: ['webgl_draw_buffers']
   })
 
+  function checkFBO (fbo, expected) {
+    var w = expected.width
+    var h = expected.height
+
+    t.equals(fbo.width, w, 'width ok')
+    t.equals(fbo.height, h, 'height ok')
+    t.equals(fbo.color[0].width, w, 'color width ok')
+    t.equals(fbo.color[0].height, h, 'color height ok')
+    t.equals(fbo.depthStencil.width, w, 'depth stencil width ok')
+    t.equals(fbo.depthStencil.height, h, 'depth stencil width ok')
+  }
+
   var fbo = regl.framebuffer(10, 10)
 
   t.equals(fbo.resize(10), fbo, 'resizing to same size does nothing')
 
-  t.equals(fbo.width, 10, 'width ok')
-  t.equals(fbo.height, 10, 'height ok')
-  t.equals(fbo.color[0].width, 10, 'color width ok')
-  t.equals(fbo.color[0].height, 10, 'color height ok')
-  t.equals(fbo.depthStencil.width, 10, 'depth stencil width ok')
-  t.equals(fbo.depthStencil.height, 10, 'depth stencil width ok')
+  checkFBO(fbo, {width: 10, height: 10})
 
   t.equals(fbo.resize(30, 5), fbo, 'resize returned the right thing')
 
-  t.equals(fbo.width, 30, 'width ok')
-  t.equals(fbo.height, 5, 'height ok')
-  t.equals(fbo.color[0].width, 30, 'color width ok')
-  t.equals(fbo.color[0].height, 5, 'color height ok')
-  t.equals(fbo.depthStencil.width, 30, 'depth stencil width ok')
-  t.equals(fbo.depthStencil.height, 5, 'depth stencil width ok')
+  checkFBO(fbo, {width: 30, height: 5})
 
   t.equals(fbo.resize(8), fbo, 'resize returned the right thing')
 
-  t.equals(fbo.width, 8, 'width ok')
-  t.equals(fbo.height, 8, 'height ok')
-  t.equals(fbo.color[0].width, 8, 'color width ok')
-  t.equals(fbo.color[0].height, 8, 'color height ok')
-  t.equals(fbo.depthStencil.width, 8, 'depth stencil width ok')
-  t.equals(fbo.depthStencil.height, 8, 'depth stencil width ok')
+  checkFBO(fbo, {width: 8, height: 8})
 
   // reinitialize fbo
 
@@ -45,23 +42,13 @@ tape('framebuffer resizing', function (t) {
     color: color
   })
 
+  checkFBO(fbo, {width: 8, height: 8})
   t.equals(fbo.color[0], color)
-  t.equals(fbo.width, 8, 'width ok')
-  t.equals(fbo.height, 8, 'height ok')
-  t.equals(fbo.color[0].width, 8, 'color width ok')
-  t.equals(fbo.color[0].height, 8, 'color height ok')
-  t.equals(fbo.depthStencil.width, 8, 'depth stencil width ok')
-  t.equals(fbo.depthStencil.height, 8, 'depth stencil width ok')
 
   fbo.resize(2, 3)
 
+  checkFBO(fbo, {width: 2, height: 3})
   t.equals(fbo.color[0], color)
-  t.equals(fbo.width, 2, 'width ok')
-  t.equals(fbo.height, 3, 'height ok')
-  t.equals(fbo.color[0].width, 2, 'color width ok')
-  t.equals(fbo.color[0].height, 3, 'color height ok')
-  t.equals(fbo.depthStencil.width, 2, 'depth stencil width ok')
-  t.equals(fbo.depthStencil.height, 3, 'depth stencil width ok')
 
   // now test .resize for MRT.
   if (regl.hasExtension('webgl_draw_buffers')) {
