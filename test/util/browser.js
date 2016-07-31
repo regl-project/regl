1e39a2c9ae5544cc45404ced969f0d4b7f8927a2
mikolalysenko <mikolalysenko@gmail.com>
2016-07-31 13:09:32 -0700

    use internal browser formatting code instead of tap-browser-color.  fixes O(n^2) performance bug in browser unit test harness

--- test/util/browser.js
+++ test/util/browser.js
@@ -1,2 +1,45 @@
-require('tap-browser-color')()
+var colors = {
+  PENDING: '#FCD62A',
+  FAILING: '#F28E82',
+  PASSING: '#8ECA6C'
+}
+
+var originalLog = console.log
+var container = document.body.appendChild(document.createElement('pre'))
+
+var failed = 0
+var passed = 0
+
+function updateStyle () {
+  var s = document.body.style
+  if (failed > 0) {
+    if (s.backgroundColor !== colors.FAILING) {
+      s.backgroundColor = colors.FAILING
+    }
+  } else if (passed > 0 && failed === 0) {
+    if (s.backgroundColor !== colors.PASSING) {
+      s.backgroundColor = colors.PASSING
+    }
+  } else {
+    if (s.backgroundColor !== colors.PENDING) {
+      s.backgroundColor = colors.PENDING
+    }
+  }
+}
+
+console.log = function (line) {
+  if (typeof line !== 'string') {
+    line = line + ''
+  }
+  if (line.indexOf('ok') === 0) {
+    passed += 1
+  } else if (line.indexOf('not ok') === 0) {
+    failed += 1
+  }
+  updateStyle()
+  originalLog.apply(console, arguments)
+  container.appendChild(document.createTextNode(line + '\n'))
+}
+
+updateStyle()
 require('./index')
