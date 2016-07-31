3918403f60fc35a3dc12dda4af25fc5b76f09753
mikolalysenko <mikolalysenko@gmail.com>
2016-07-31 13:52:02 -0700

    add test case for cube map resizing

---
+++ test/cube-fbo-resize.js
@@ -0,0 +1,20 @@
+var createContext = require('./util/create-context')
+var createREGL = require('../../regl')
+var tape = require('tape')
+
+tape('cube fbo resize test', function (t) {
+  var gl = createContext(2, 2)
+  var regl = createREGL(gl)
+
+  t.equals(gl.getError(), 0, 'error code ok')
+
+  var cubeFbo = regl.framebufferCube(10)
+  t.equals(gl.getError(), 0, 'error code ok')
+
+  cubeFbo.resize(8)
+  t.equals(gl.getError(), 0, 'error code ok')
+
+  regl.destroy()
+  createContext.destroy(gl)
+  t.end()
+})
