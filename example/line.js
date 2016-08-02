28c92e0873955dcd5a2dc85340b12a98c017270d
Jay Weeks <jay.patrick.weeks@gmail.com>
2016-08-01 23:36:00 -0700

    Add line example description

--- example/line.js
+++ example/line.js
@@ -1,3 +1,11 @@
+/*
+  <p>This example demonstrates rendering screen space projected lines
+  from a technique described in https://mattdesl.svbtle.com/drawing-lines-is-hard.</p>
+
+  <p>This technique requires each vertex to reference the previous and next vertex in the line;
+  this example utilizes attribute byte offsets to share a single position buffer for all three
+  of these attributes.</p>
+*/
 const createRegl = require('../regl')
 const mat4 = require('gl-mat4')
 const createCamera = require('canvas-orbit-camera')
