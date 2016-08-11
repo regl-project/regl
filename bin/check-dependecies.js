f9a7c3d57aaa8ba89b5050f174373304de6e2efd
thammin <rim_paulyoung@cyberagent.co.jp>
2016-08-11 11:13:34 +0900

    port `check-dependencies.js` to ES5

--- bin/check-dependecies.js
+++ bin/check-dependecies.js
@@ -1,34 +1,33 @@
-const depCheck = require('dependency-check')
-const deglob = require('deglob')
-const path = require('path')
+var depCheck = require('dependency-check')
+var deglob = require('deglob')
+var path = require('path')
 
-const ROOT = path.join(__dirname, '..')
+var ROOT = path.join(__dirname, '..')
 
 // reference: https://github.com/flet/deglob
 // Find all js files but exclude
 // - patterns defined in `.gitignore`
 // - ignore list defined in `package.json`
-const option = {
+var option = {
   configKey: 'standard'
 }
 
-deglob('**/*.js', option, (err, files) => {
+deglob('**/*.js', option, function (err, files) {
   if (err) throw err
 
   depCheck({
     path: ROOT,
     entries: files.map(path.relative.bind(null, ROOT)),
     noDefaultEntries: true
-  }, (err, data) => {
+  }, function (err, data) {
     if (err) throw err
 
     // check missing dependencies
     var result = depCheck.missing(data.package, data.used)
 
-    result.length && console.error(`
-      Dependencies not listed in package.json:
-        ${result.join('\n\t')}
-    `)
+    if (result.length) {
+      console.error(['Dependencies not listed in package.json:'].concat(result).join('\n\t') + '\n')
+    }
     process.exit(result.length)
   })
 })
