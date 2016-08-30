cbf75380bee3b24a39e2cd8f4be8909c3e96c5ef
Andrew Taber <andrew.e.taber@gmail.com>
2016-08-30 15:00:02 -0700

    Split up metaball shader for better readability.

--- example/metaball.js
+++ example/metaball.js
@@ -16,7 +16,7 @@ const camera = require('./util/camera')(regl, {
   theta: 1.0
 })
 
-let maxCount = 4096
+const maxCount = 4096
 
 const positionBuffer = regl.buffer({
   length: maxCount * 3 * 4,
@@ -115,13 +115,29 @@ const drawMetaballs = regl({
       return fract(sin(dot(gl_FragCoord.xyz + seed,scale)) * 43758.5453 + seed);
     }
 
-    void main() {
-      vec3 n = normalize(vONormal.xyz);
-      vec3 blend_weights = abs(n);
-      blend_weights = (blend_weights - 0.2) * 7.;
-      blend_weights = max(blend_weights, 0.);
-      blend_weights /= (blend_weights.x + blend_weights.y + blend_weights.z);
+    vec3 spherical_environment_map(vec3 ray, vec3 normal) {
+      vec3 r = reflect(normalize(ray), normalize(normal));
+      float m = 2.0 * sqrt(r.x * r.x + r.y * r.y + (r.z + 1.0) * (r.z + 1.0));
+      vec2 calculatedNormal = vec2(r.x / m + 0.5, r.y / m + 0.5);
+      return texture2D(textureMap, calculatedNormal).rgb;
+    }
 
+    mat3 tangent_space_basis(vec3 blend_weights) {
+      vec3 tanX = vec3(vNormal.x, -vNormal.z, vNormal.y);
+      vec3 tanY = vec3(vNormal.z, vNormal.y, -vNormal.x);
+      vec3 tanZ = vec3(-vNormal.y, vNormal.x, vNormal.z);
+      vec3 blended_tangent = tanX * blend_weights.xxx +
+                             tanY * blend_weights.yyy +
+                             tanZ * blend_weights.zzz;
+
+      return mat3(
+        normalize(blended_tangent),
+        normalize(cross(vNormal, blended_tangent)),
+        normalize(vNormal)
+      );
+    }
+
+    vec3 blended_bump(vec3 blend_weights) {
       vec2 coord1 = vPosition.yz * texScale;
       vec2 coord2 = vPosition.zx * texScale;
       vec2 coord3 = vPosition.xy * texScale;
@@ -130,37 +146,34 @@ const drawMetaballs = regl({
       vec3 bump2 = texture2D(normalMap, coord2).rgb;
       vec3 bump3 = texture2D(normalMap, coord3).rgb;
 
-      vec3 blended_bump = bump1 * blend_weights.xxx +
-                          bump2 * blend_weights.yyy +
-                          bump3 * blend_weights.zzz;
+      return bump1 * blend_weights.xxx +
+             bump2 * blend_weights.yyy +
+             bump3 * blend_weights.zzz;
+    }
 
-      vec3 tanX = vec3(vNormal.x, -vNormal.z, vNormal.y);
-      vec3 tanY = vec3(vNormal.z, vNormal.y, -vNormal.x);
-      vec3 tanZ = vec3(-vNormal.y, vNormal.x, vNormal.z);
-      vec3 blended_tangent = tanX * blend_weights.xxx +
-                             tanY * blend_weights.yyy +
-                             tanZ * blend_weights.zzz;
+    void main() {
+      vec3 blend_weights = abs(normalize(vONormal.xyz));
+      blend_weights = (blend_weights - 0.2) * 7.;
+      blend_weights = max(blend_weights, 0.);
+      blend_weights /= (blend_weights.x + blend_weights.y + blend_weights.z);
 
-      vec3 normalTex = blended_bump * 2.0 - 1.0;
+      mat3 tsb = tangent_space_basis(blend_weights);
+      vec3 bump = blended_bump(blend_weights);
+
+      vec3 normalTex = bump * 2.0 - 1.0;
       normalTex.xy *= normalScale;
       normalTex.y *= -1.;
       normalTex = normalize(normalTex);
-      mat3 tsb = mat3(normalize(blended_tangent), normalize(cross(vNormal, blended_tangent)), normalize(vNormal));
       vec3 finalNormal = tsb * normalTex;
-
-      vec3 r = reflect(normalize(vU), normalize(finalNormal));
-      float m = 2.0 * sqrt(r.x * r.x + r.y * r.y + (r.z + 1.0) * (r.z + 1.0));
-      vec2 calculatedNormal = vec2(r.x / m + 0.5, r.y / m + 0.5);
-
-      vec3 base = texture2D(textureMap, calculatedNormal).rgb;
+      vec3 base = spherical_environment_map(vU, finalNormal);
 
       float rim = 1.75 * max(0., abs(dot(normalize(vNormal), normalize(-vOPosition.xyz))));
       base += 10. * base * color * clamp(1. - rim, 0., .15);
 
       base = vec3(1.) - (vec3(1.) - base) * (vec3(1.) - base);
 
-      float nn = .05 * random(vec3(1.), length(gl_FragCoord));
-      base += vec3(nn);
+      float dither = .05 * random(vec3(1.), length(gl_FragCoord));
+      base += vec3(dither);
 
       gl_FragColor = vec4(base.rgb, 1.);
     }`,
