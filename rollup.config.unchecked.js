a561aac02402e818b78679c6a699cd64db31a6eb
Rich-Harris <richard.a.harris@gmail.com>
2016-10-08 17:01:40 -0400

    lint

--- rollup.config.unchecked.js
+++ rollup.config.unchecked.js
@@ -2,6 +2,6 @@ var config = require('./rollup.config.js')
 var removeCheck = require('./bin/remove-check.js')
 
 config.dest = 'dist/regl.unchecked.js'
-config.plugins.push( removeCheck() );
+config.plugins.push(removeCheck())
 
-export default config;
+export default config
