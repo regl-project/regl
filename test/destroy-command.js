29c922ce19afaa5b02141e5c10c9384e6e5a4fa8
fuzhenn <fuzhen@maptalks.org>
2020-05-22 11:53:07 +0800

    add refCount to REGLProgram

--- test/destroy-command.js
+++ test/destroy-command.js
@@ -38,9 +38,42 @@ tape('destroy command', function (t) {
     count: 3
   })
 
+  // create a command with the same vert and frag
+  var anotherCommand = regl({
+    vert: [
+      'precision mediump float;',
+      'attribute vec2 a1;',
+      'attribute vec3 a2;',
+      'varying vec4 color;',
+      'void main() {',
+      '  gl_Position = vec4(a1, 0, 1);',
+      '  color = vec4(a2, 1);',
+      '}'
+    ].join('\n'),
+
+    frag: [
+      'precision mediump float;',
+      'varying vec4 color;',
+      'void main() {',
+      '  gl_FragColor = color;',
+      '}'
+    ].join('\n'),
+
+    attributes: {
+      a1: a1,
+      a2: a2
+    },
+
+    primitive: 'points',
+    count: 3
+  })
+
+  command.destroy()
+  // should be safe to call
+  anotherCommand()
   a1.destroy()
   a2.destroy()
-  command.destroy()
+  anotherCommand.destroy()
 
   regl.destroy()
   t.equals(gl.getError(), 0, 'error ok')
